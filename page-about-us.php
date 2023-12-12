c7a8437d09cdfd5f8d90fa0639dec06dbecdff94
danelian <141317269+danelian@users.noreply.github.com>
2023-12-12 04:16:21 +0300

    block our indicators

--- page-about-us.php
+++ page-about-us.php
@@ -31,37 +31,43 @@ get_header(); ?>
     </div>
 
     <div class="aboutus-indi">
-      <h2 class="section-title">Our indicators</h2>
+      <h2 class="section-title"><?php the_field('a_indi_title'); ?></h2>
       <div class="aboutus-indi__container">
-        <div class="aboutus-indi__left">
-          <h3>Reach your financial goals</h3>
-          <p>Personal credit scoring is the application of financial risk forecasting. It becomes an even important task as financial institutions have been experiencing serious competition and challenges.</p>
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/about-us-indicators-chart-01.svg" alt="">
-        </div>
-        <div class="aboutus-indi__center">
-          <h6>78%</h6>
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/about-us-indicators-chart-02.svg" alt="">
-        </div>
-        <div class="aboutus-indi__right">
-          <div class="point">
-            <div class="point__icon"></div>
-            <div class="point__text">
-              <p>Your credit score can determine how much your loan and credit card applications are approved. Not only that, with a credit score you are given the freedom to choose the bank and lender that provides the best offer.</p>
-            </div>
+
+        <?php
+        $a_indi_left = get_field('a_indi_left');
+        if( $a_indi_left ): ?>
+          <div class="aboutus-indi__left">
+            <?php if ($a_indi_left['title']) : ?><h3><?php echo $a_indi_left['title']; ?></h3><?php endif; ?>
+            <?php if ($a_indi_left['text']) : ?><p><?php echo $a_indi_left['text']; ?></p><?php endif; ?>
+            <?php if ($a_indi_left['image']) : ?>
+              <img src="<?php echo $a_indi_left['image']; ?>" alt="chart image">
+            <?php endif; ?>
           </div>
-          <div class="point">
-            <div class="point__icon"></div>
-            <div class="point__text">
-              <p>Credit can be complicated. But it doesn't have to be. Learning responsible credit behavior can set you up to reach your goals. Find out what's in your Equifax credit scores are calculate.</p>
-            </div>
+        <?php endif; ?>
+
+        <?php
+        $a_indi_center = get_field('a_indi_center');
+        if( $a_indi_center ): ?>
+          <div class="aboutus-indi__center">
+            <?php if ($a_indi_center['percent']) : ?><h6><?php echo $a_indi_center['percent'] ?></h6><?php endif; ?>
+            <?php if ($a_indi_center['image']) : ?>
+              <img src="<?php echo $a_indi_center['image'] ?>" alt="chart image">
+            <?php endif; ?>
           </div>
-          <div class="point">
-            <div class="point__icon"></div>
-            <div class="point__text">
-              <p>Personal credit scoring is the application of financial risk forecasting. It becomes an even important task as financial institutions have been experiencing serious competition and challenges.</p>
+        <?php endif; ?>
+
+        <?php if( have_rows('a_indi_right') ): ?>
+          <div class="aboutus-indi__right">
+          <?php while( have_rows('a_indi_right') ): the_row(); ?>
+            <div class="point">
+              <div class="point__icon"></div>
+              <div class="point__text"><?php the_sub_field('text'); ?></div>
             </div>
+          <?php endwhile; ?>
           </div>
-        </div>
+        <?php endif; ?>
+
       </div>
     </div>
 
